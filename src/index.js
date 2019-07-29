f483b01e8f0fa7277a1f569b8eb550984ea6868e
renan.sugiyama <renan.sugiyama@gmail.com>
2019-07-29 17:01:34 -0300

    adicona modal para confirmar delete de receita - fixes #4

--- src/index.js
+++ src/index.js
@@ -10,11 +10,19 @@ document.querySelector('#btnAddReceita').addEventListener('click', () => {
     location.assign(`/edit.html#${id}`)
 })
 
-receitas.forEach(receita => {
-    document.querySelector(`#${receita.id}`).addEventListener('click', () => {
-        console.log('id', receita.id)
-        removeReceita(receita.id)
-        location.assign('/')
-    })
+// receitas.forEach(receita => {
+//     document.querySelector(`#${receita.id}`).addEventListener('click', () => {
+//         console.log('id', receita.id)
+//         //removeReceita(receita.id)
+//         //location.assign('/')
+//     })
 
-});
+// });
+
+$('#modalDeleteReceita').on('show.bs.modal', function(e) {
+    const receitaId = e.relatedTarget.id
+    document.querySelector("#modalBtnDeletar").onclick = () =>{
+        removeReceita(receitaId)
+        location.assign('/')
+    }
+})
